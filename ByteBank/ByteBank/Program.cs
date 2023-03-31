64803bc757a002a6e733b5f6da3a3f92b7513b10
Pedro <pedro.enrique1599@icloud.com>
2023-03-31 06:57:39 -0300

    code refactoring

--- ByteBank/ByteBank/Program.cs
+++ ByteBank/ByteBank/Program.cs
@@ -1,13 +1,19 @@
 ﻿using ByteBank;
 
-Account account01 = new Account();
+class Program
+{
+    static void Main(string[] args)
+    {
+        Account account01 = new Account();
 
-account01.accountHolder = "Person 01";
-account01.AgencyNumber = 15;
-account01.accountNumber = "1011-9";
-account01.balance = 1000;
+        account01.accountHolder = "Person 01";
+        account01.AgencyNumber = 15;
+        account01.accountNumber = "1011-9";
+        account01.balance = 1000;
 
-Console.WriteLine(account01.accountHolder + " account balance: $" + account01.balance);
+        Console.WriteLine(account01.accountHolder + " account balance: $" + account01.balance + ".");
 
-Console.Write("\nPress any key to close...");
-Console.ReadLine();
+        Console.Write("\nPress any key to close...");
+        Console.ReadLine();
+    }
+}
