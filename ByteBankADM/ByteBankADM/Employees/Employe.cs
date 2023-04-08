be831cbc314b2119501c77f26fdd6646572ccc24
Pedro <pedro.enrique1599@icloud.com>
2023-04-07 23:37:23 -0300

    C# bank algorithm Project

---
+++ ByteBankADM/ByteBankADM/Employees/Employe.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ByteBankADM.Employees
+{
+    public class Employe
+    {
+        public string Name { get; set; }
+        public string ID { get; set; }
+        public double Salary { get; set; }
+
+        public double GetBonus()
+        {
+            return this.Salary * 0.1;
+        }
+    }
+}
---
+++ ByteBankADM/ByteBankADM/Program.cs
@@ -0,0 +1,23 @@
+﻿using ByteBankADM.Employees;
+using System;
+
+namespace ByteBankADM
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Employe employe = new Employe();
+
+            employe.Name = "Employe 01";
+            employe.ID = "135.747.361-78";
+            employe.Salary = 2000;
+
+            Console.WriteLine("Employe name: " + employe.Name);
+            Console.WriteLine("Employe salary bonus: " + employe.GetBonus());
+
+            Console.Write("\nPress any key to close...");
+            Console.ReadLine();
+        }
+    }
+}
