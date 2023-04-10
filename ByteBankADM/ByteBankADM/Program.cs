063ff21be408d47636cb84ccf2acb5a505756ce0
Pedro <pedro.enrique1599@icloud.com>
2023-04-09 23:55:27 -0300

    new offices

--- ByteBankADM/ByteBankADM/Program.cs
+++ ByteBankADM/ByteBankADM/Program.cs
@@ -1,5 +1,6 @@
 ﻿using ByteBankADM.Employees;
 using System;
+using System.Security.Cryptography.X509Certificates;
 
 namespace ByteBankADM
 {
@@ -7,14 +8,30 @@ namespace ByteBankADM
     {
         static void Main(string[] args)
         {
+
+            void Linebreak()
+            {
+                Console.WriteLine("\n");
+            } 
+
             Employe employe = new Employe();
 
-            employe.Name = "Employe 01";
+            employe.Name = "Louie Duck";
             employe.ID = "135.747.361-78";
             employe.Salary = 2000;
 
             Console.WriteLine("Employe name: " + employe.Name);
-            Console.WriteLine("Employe salary bonus: " + employe.GetBonus());
+            Console.WriteLine("Employe salary bonus: $" + employe.GetBonus());
+
+            Linebreak();
+
+            CEO director = new CEO();
+            director.Name = "Scrooge McDuck";
+            director.ID = "047.341.881-18";
+            director.Salary = 150000;
+
+            Console.WriteLine("CEO name: " + director.Name);
+            Console.WriteLine("CEO salary bonus: $" + director.GetBonus());
 
             Console.Write("\nPress any key to close...");
             Console.ReadLine();
